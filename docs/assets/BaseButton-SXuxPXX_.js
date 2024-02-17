d722e0f903734b21b928f37f397e43f362b8214c
Learde <leardeleo@gmail.com>
2024-02-17 22:40:47 +0300

    build: rebuild project for gh pages

---
+++ docs/assets/BaseButton-SXuxPXX_.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as s,r as n}from"./index-pjyOXBHH.js";const c={},r={class:"button"};function a(e,_){return o(),s("button",r,[n(e.$slots,"default",{},void 0,!0)])}const u=t(c,[["render",a],["__scopeId","data-v-753d214a"]]);export{u as B};
