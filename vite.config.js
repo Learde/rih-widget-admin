d9f30387d5cea7337c6f4221c3241700a1347eb4
Sasha <sasha-gezha1@mail.ru>
2024-02-21 08:34:39 +0300

    vk tunnel fix

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { URL, fileURLToPath } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), mkcert()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
